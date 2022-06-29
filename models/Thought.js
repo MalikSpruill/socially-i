a207fe9118862b2d77d4d05b5231b58438feb9a7
Malik <malikspruill@gmail.com>
2022-06-28 20:01:37 -0400

    Add Thought Schema in models sub-directory

--- models/Thought.js
+++ models/Thought.js
@@ -28,3 +28,38 @@ const ReactionSchema = new Schema({
 }
 );
 
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdTme => dateFormat(createdTme)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
+
